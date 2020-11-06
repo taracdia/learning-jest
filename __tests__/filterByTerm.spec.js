3e962d7d9aa0cadb839f33139738c0fc67935d40
Tara Dia <taracdia@gmail.com>
2020-11-05 20:08:37 -0600

    added error conditions

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,13 +1,13 @@
 const filterByTerm = require("../src/filterByTerm");
 
 describe("Filter function", () => {
-    test("should filter by search term", () => {
-        const input = [
-            { id: 1, url: "https://www.url1.dev" },
-            { id: 2, url: "https://www.url2.dev" },
-            { id: 3, url: "https://www.link3.dev" }
-        ];
+    const input = [
+        { id: 1, url: "https://www.url1.dev" },
+        { id: 2, url: "https://www.url2.dev" },
+        { id: 3, url: "https://www.link3.dev" }
+    ];
 
+    test("should filter by search term 'link/LINK'", () => {
         const output1 = [{ id: 3, url: "https://www.link3.dev" }];
         expect(filterByTerm(input, "link")).toEqual(output1);
 
@@ -15,7 +15,11 @@ describe("Filter function", () => {
 
         expect(filterByTerm(input, "uRl")).toEqual([{ id: 1, url: "https://www.url1.dev" },
         { id: 2, url: "https://www.url2.dev" }]);
+    });
 
-        expect(filterByTerm(input, "")).toEqual(new Error("searchTerm cannot be empty"));
+    test("throw errors", () => {
+        expect(() => filterByTerm(input, "")).toThrow("searchTerm cannot be empty");
+
+        expect(() => filterByTerm([], "link")).toThrow("inputArr cannot be empty");
     });
 });
