6347d776259709a8238e0fa866dcd92e9077d359
Tara Dia <taracdia@gmail.com>
2020-11-05 19:20:52 -0600

    added tests

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,12 +1,29 @@
 describe("Filter function", () => {
-    test("should filter by search term 'link'", () => {
+    test("should filter by search term", () => {
         const input = [
             { id: 1, url: "https://www.url1.dev" },
             { id: 2, url: "https://www.url2.dev" },
             { id: 3, url: "https://www.link3.dev" }
         ];
 
-        const output = [{ id: 3, url: "https://www.link3.dev" }];
-        expect(filterByTerm(input, "link")).toEqual(output);
+        const output1 = [{ id: 3, url: "https://www.link3.dev" }];
+        expect(filterByTerm(input, "link")).toEqual(output1);
+
+        expect(filterByTerm(input, "LINK")).toEqual(output1);
+
+        expect(filterByTerm(input, "uRl")).toEqual([{ id: 1, url: "https://www.url1.dev" },
+        { id: 2, url: "https://www.url2.dev" }]);
+
+        expect(filterByTerm(input, "")).toEqual([]);
+
+
     });
 });
+
+filterByTerm = (inputArr, searchTerm) => {
+    if(searchTerm){
+        return inputArr.filter(item => item.url.match(new RegExp(searchTerm, "i")));
+    } else {
+        return [];
+    }
+}
